8f0b9e7b2b07dc88c454f3eff3ed50c17711b28d
EyaGratiTDS <eya.grati@tds-technologies.fr>
2025-08-20 18:35:17 +0100

    Update Project

--- vite.config.ts
+++ vite.config.ts
@@ -4,67 +4,76 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [react(),
-    VitePWA({
-        registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
-      srcDir: 'src',
-      filename: 'sw.js', // Changed to .js
-      strategies: 'injectManifest',
-      injectRegister: false,
-      manifest: {
-        name: 'NexCard',
-        short_name: 'NexCard',
-        description: 'The new generation of connected cards',
-        theme_color: '#06A3DA',
-        background_color: '#ffffff',
-        display: 'standalone',
-        start_url: '/',
-        icons: [
-          {
-            src: '/img/Logo-NexCard-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/img/Logo-NexCard-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: '/img/Logo-NexCard-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
+  VitePWA({
+    registerType: 'autoUpdate',
+    includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+    srcDir: 'src',
+    filename: 'sw.js', // Changed to .js
+    strategies: 'injectManifest',
+    injectRegister: false,
+    manifest: {
+      name: 'NexCard',
+      short_name: 'NexCard',
+      description: 'The new generation of connected cards',
+      theme_color: '#06A3DA',
+      background_color: '#ffffff',
+      display: 'standalone',
+      start_url: '/',
+      icons: [
+        {
+          src: '/img/Logo-NexCard-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'any'
+        },
+        {
+          src: '/img/Logo-NexCard-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any'
+        },
+        {
+          src: '/img/Logo-NexCard-maskable-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'maskable'
+        },
+        {
+          src: '/img/Logo-NexCard-maskable-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable'
+        }
+      ]
+
+    },
+    workbox: {
+      runtimeCaching: [
+        {
+          urlPattern: ({ request }) => request.destination === 'document',
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'html-cache'
           }
-        ]
-      },
-      workbox: {
-        runtimeCaching: [
-          {
-            urlPattern: ({ request }) => request.destination === 'document',
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'html-cache'
-            }
-          },
-          {
-            urlPattern: ({ request }) => request.destination === 'image',
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'image-cache',
-              expiration: {
-                maxEntries: 50,
-                maxAgeSeconds: 60 * 60 * 24 * 30 
-              }
+        },
+        {
+          urlPattern: ({ request }) => request.destination === 'image',
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'image-cache',
+            expiration: {
+              maxEntries: 50,
+              maxAgeSeconds: 60 * 60 * 24 * 30
             }
           }
-        ]
-      },
-      devOptions: {
-        enabled: true,
-        type: 'module', // Add this for dev support
-      }
-    })
+        }
+      ]
+    },
+    devOptions: {
+      enabled: true,
+      type: 'module', // Add this for dev support
+    }
+  })
   ],
   build: {
     rollupOptions: {
